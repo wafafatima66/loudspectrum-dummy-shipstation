590a807160f45c4e9fa28440956a5914378afd0b
wafafatima66 <wafafatima66@gmail.com>
2025-11-04 00:27:03 +0600

    initial commit

--- app/api/shipstation/route.js
+++ app/api/shipstation/route.js
@@ -1,9 +1,37 @@
 export async function GET() {
   const xmlResponse = `<?xml version="1.0" encoding="utf-8"?>
-  <StoreResponse>
-    <StoreName>Loud Spectrum</StoreName>
-    <Orders></Orders>
-  </StoreResponse>`;
+<Orders>
+  <Order>
+    <OrderID>1</OrderID>
+    <OrderNumber>TEST-001</OrderNumber>
+    <OrderDate>2025-11-02T00:00:00</OrderDate>
+    <OrderStatus>awaiting_shipment</OrderStatus>
+    <LastModifiedDate>2025-11-02T00:00:00</LastModifiedDate>
+    <ShippingMethod>Standard</ShippingMethod>
+    <OrderTotal>0.00</OrderTotal>
+    <CustomerNotes>Test connection OK</CustomerNotes>
+    <InternalNotes>Dummy data for ShipStation validation</InternalNotes>
+    <BillTo>
+      <Name>Test User</Name>
+    </BillTo>
+    <ShipTo>
+      <Name>Test User</Name>
+      <Street1>123 Main St</Street1>
+      <City>Austin</City>
+      <State>TX</State>
+      <PostalCode>78701</PostalCode>
+      <Country>US</Country>
+    </ShipTo>
+    <Items>
+      <Item>
+        <SKU>TESTSKU</SKU>
+        <Name>Validation Item</Name>
+        <Quantity>1</Quantity>
+        <UnitPrice>0.00</UnitPrice>
+      </Item>
+    </Items>
+  </Order>
+</Orders>`;
 
   return new Response(xmlResponse, {
     headers: {
